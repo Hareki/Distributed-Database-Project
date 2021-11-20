0c4361ce19cf09b345c34d3b308010222a286c64
Minh Tu <minhtu1392000@gmail.com>
2021-11-20 14:28:14 +0700

    update

---
+++ TracNghiemCSDLPT/Others/ConfirmForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace TracNghiemCSDLPT.Others
+{
+    partial class ConfirmForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureClose = new System.Windows.Forms.PictureBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.message = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureClose
+            // 
+            this.pictureClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureClose.Image = global::TracNghiemCSDLPT.Properties.Resources.cancel_35px;
+            this.pictureClose.Location = new System.Drawing.Point(361, 12);
+            this.pictureClose.Name = "pictureClose";
+            this.pictureClose.Size = new System.Drawing.Size(37, 50);
+            this.pictureClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureClose.TabIndex = 6;
+            this.pictureClose.TabStop = false;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::TracNghiemCSDLPT.Properties.Resources.ok_45px;
+            this.pictureBox.Location = new System.Drawing.Point(12, 52);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(42, 48);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 5;
+            this.pictureBox.TabStop = false;
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.Font = new System.Drawing.Font("Baloo 2", 16.2F);
+            this.message.ForeColor = System.Drawing.Color.White;
+            this.message.Location = new System.Drawing.Point(60, 52);
+            this.message.MaximumSize = new System.Drawing.Size(350, 0);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(185, 45);
+            this.message.TabIndex = 4;
+            this.message.Text = "Message Text";
+            // 
+            // ConfirmForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(410, 170);
+            this.Controls.Add(this.pictureClose);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.message);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ConfirmForm";
+            this.Text = "ConfirmForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureClose;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label message;
+    }
+}
